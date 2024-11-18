bce8d3e140f607974daa91837b4023612a2d3373
Peter <csepemail@gmail.com>
2024-11-18 15:15:24 +0100

    DAP demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dap-demo';
+  variant = 'h1'
+
+  onDDSInputChanged(event: CustomEvent) {
+    console.log(event.detail.value);
+  }
 }
